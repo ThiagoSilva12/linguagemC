8f04497993135f1ba731127efe7f4cdba90eb5a9
ThiagoSilva12 <79534413+ThiagoSilva12@users.noreply.github.com>
2021-03-04 20:49:46 -0300

    atualizei aula3

---
+++ aula3/aula3/média/media2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+	Cálculo da média
+	@author Thiago Silva Campos
+*/
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL,"Portuguese");
+	printf("Cálculo da média\n");
+	//variáveis
+	float nota1,nota2,media;
+	//entrada de dados
+	printf("Digite a nota 1: ");
+	scanf("%f",&nota1);
+	printf("Digite a nota 2: ");
+	scanf("%f",&nota2);
+	//processamento
+	media = (nota1 + nota2) /2;
+	//saída
+	printf("Resultado: %.1f\n", media);
+	//status
+	if(media < 5){
+		printf("REPROVADO\n");
+	} else {
+		printf("APROVADO\n");
+	}
+	system("pause");
+	return 0;
+}
