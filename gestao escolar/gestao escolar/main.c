2b9267b8878dbf4c7335cb4daadac42589f0fe4d
ThiagoSilva12 <79534413+ThiagoSilva12@users.noreply.github.com>
2021-03-04 20:51:49 -0300

    atualizei aula4

---
+++ gestao escolar/gestao escolar/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/* 
+	Gestão Escolar
+	@author Thiago Silva Campos
+ */
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL,"Portuguese");
+	printf("Gestão escolar\n");
+	char professora[30]=("Maria Eduarda Silva Merces"); //[30] -> máximo de 30 caracter
+	int periodo = manhã;
+	float disciplina:Língua Portuguesa;
+	printf("Professora %s\n", professora);
+	printf("Periodo %d\n",periodo);
+	printf("Disciplina %f\n",disciplina);
+	system("pause");
+	System("cls");
+	//Cadastro do aluno
+	char aluno[30]
+	char sexo = 'm/f';
+	int idade;
+	float imc,peso,altura
+	printf("Aluno: %s\n",aluno);
+	printf("Sexo: ");
+	system("pause");
+	system("cls");
+	//Entada de dados
+	printf("Cadastro do aluno\n");
+	printf("Nome do aluno: \n");
+	printf("Idade: \n");
+	scanf("%f",&idade);
+	printf("Sexo(M/F): \n");
+	scanf("%f",&sexo);
+	system("pause");
+	system("cls");
+	return 0;
+}
